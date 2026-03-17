0499609d6c20d375aaecf59a1b05e2ab54447c85
yeahn <yeahn1107@gmail.com>
2026-03-17 14:46:13 +0900

    ehcache 걷어내고 caffeine 사용하기

--- src/main/java/com/yeahn/cache/CacheConfig.java
+++ src/main/java/com/yeahn/cache/CacheConfig.java
@@ -1,8 +1,28 @@
 package com.yeahn.cache;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 @EnableCaching
 @Configuration
-public class CacheConfig {}
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("getMenuList");
+
+        cacheManager.setCaffeine(
+                Caffeine.newBuilder()
+                        .maximumSize(1000)
+                        .expireAfterWrite(1, TimeUnit.HOURS)
+        );
+
+        return cacheManager;
+    }
+}
