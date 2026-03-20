5e6ce17114425967c85526aee0de8741e8c47744
yeahn <yeahn1107@gmail.com>
2026-03-20 20:50:49 +0900

    운동 템플릿 레포지토리

---
+++ src/main/java/com/yeahn/template/dao/ExerciseTemplateMapper.java
@@ -0,0 +1,9 @@
+package com.yeahn.template.dao;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class ExerciseTemplateMapper {
+}
