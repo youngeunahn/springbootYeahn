c1928dc2aa21089178ec8935495fa4acca78f05c
yeahn <yeahn1107@gmail.com>
2026-04-24 17:12:11 +0900

    인터셉터 애노테이션 변경

--- src/main/java/com/yeahn/config/WebMvcConfigurer.java
+++ src/main/java/com/yeahn/config/WebMvcConfigurer.java
@@ -1,26 +1,18 @@
 package com.yeahn.config;
 
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-
-import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebMvcConfigurer implements org.springframework.web.servlet.config.annotation.WebMvcConfigurer {
 
-	@Bean
-	public WebInterceptor webInterceptor() {
-		return new WebInterceptor();
-	}
+	@Autowired
+	private WebInterceptor webInterceptor;
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(webInterceptor())
+		registry.addInterceptor(webInterceptor)
 				.addPathPatterns("", "/", "/**") //해당경로 접근전에 인터셉터가 가로첸다
 				.excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.map")
 				.excludePathPatterns("/ajax/**/*", "/api/**/*");
